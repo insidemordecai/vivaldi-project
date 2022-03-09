7064d8a1c1e1612f43a261d7ea7a53412fd62318
itsmordecai <mordecai.kipngetich@gmail.com>
2022-03-09 10:17:19 +0300

    create dashboard and header

--- dashboard.php
+++ dashboard.php
@@ -1,9 +1,239 @@
 <?php
 
+session_start();
 include "handle_glogin.php";
+include "handle_login.php";
+include "header.php";
 
-echo $_SESSION["firstname"];
-echo $_SESSION["secondname"];
-echo $_SESSION["email"];
+if (!isset($_SESSION["loggedin"]) or $_SESSION["loggedin"] !== true) {
+  header("location: index.php");
+  exit();
+}
 
-echo "<img src='" . $_SESSION["picture"] . "' >";
+?>
+
+<div class="row m-2 p-2">
+  <div class="col-md-6">
+    <h3 class="grey">The Dashboard</h3>
+  </div>
+
+  <div class="col-md-6">
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary float-end" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      Get Report
+      <i class="fa fa-arrow-circle-o-down fa-lg"></i>
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">
+              Choose a Report
+            </h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">...</div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+              Close
+            </button>
+            <button type="button" class="btn btn-primary">
+              Save changes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row m-2">
+  <div class="col">
+    <div class="card">
+      <div class="card-body border-left-primary">
+        <div class="row align-items-center">
+          <div class="col">
+            <div class="text-primary">Requests</div>
+            <div class="grey">50+ requests</div>
+          </div>
+          <div class="col-auto">
+            <i class="fa fa-comments-o fa-2x grey"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col">
+    <div class="card">
+      <div class="card-body border-left-green">
+        <div class="row align-items-center">
+          <div class="col">
+            <div class="text-success">Monthly Earnings</div>
+            <div class="grey">$400,000</div>
+          </div>
+          <div class="col-auto">
+            <i class="fa fa-calendar-o fa-2x grey"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col">
+    <div class="card">
+      <div class="card-body border-left-orange">
+        <div class="row align-items-center">
+          <div class="col">
+            <div class="text-warning">To Do List</div>
+            <div class="grey">10 Pending</div>
+          </div>
+          <div class="col-auto">
+            <i class="fa fa-reorder fa-2x grey"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row m-2">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-header text-primary bg-white">
+        Projects Progress
+      </div>
+      <div class="card-body">
+        <div class="m-2 p-2">
+          <span class="grey">Software Installation</span>
+          <div class="progress">
+            <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+              25%
+            </div>
+          </div>
+        </div>
+        <div class="m-2 p-2">
+          <span class="grey">Account Setup</span>
+          <div class="progress">
+            <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="58" aria-valuemin="0" aria-valuemax="100">
+              58%
+            </div>
+          </div>
+        </div>
+        <div class="m-2 p-2">
+          <span class="grey">Dashboard Design</span>
+          <div class="progress">
+            <div class="progress-bar bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+              75%
+            </div>
+          </div>
+        </div>
+        <div class="m-2 p-2">
+          <span class="grey">Dashboard Creation</span>
+          <div class="progress">
+            <div class="progress-bar bg-danger" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+              100%
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-header text-primary bg-white">
+        Toasts and Tooltiops
+      </div>
+      <div class="card-body">
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Mollitia accusantium asperiores accusamus. Facilis magni
+          impedit nihil explicabo itaque quos fugit?
+          <a href="" data-bs-toggle="tooltip" data-bs-placement="bottom" title="This link will take you to mysite.com">Lorem ipsum dolor sit amet</a>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Omnis ducimus magni saepe nemo quod itaque reiciendis cumque
+          tenetur dolores a.
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row m-2">
+  <div class="col-6">
+    <div class="row">
+      <div class="col">
+        <div class="alert alert-primary" role="alert">
+          A simple primary alert—check it out!
+        </div>
+      </div>
+      <div class="col">
+        <div class="alert alert-secondary" role="alert">
+          A simple secondary alert—check it out!
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="alert alert-success" role="alert">
+          A simple success alert—check it out!
+        </div>
+      </div>
+      <div class="col">
+        <div class="alert alert-warning" role="alert">
+          A simple warning alert—check it out!
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="alert alert-danger" role="alert">
+          A simple danger alert—check it out!
+        </div>
+      </div>
+      <div class="col">
+        <div class="alert alert-dark" role="alert">
+          A simple dark alert—check it out!
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-6">
+    <ul class="list-group">
+      <li class="list-group-item d-flex justify-content-between">
+        A list item
+        <span class="badge bg-primary rounded-pill">14</span>
+      </li>
+      <li class="list-group-item d-flex justify-content-between">
+        A second list item
+        <span class="badge bg-primary rounded-pill">2</span>
+      </li>
+      <li class="list-group-item d-flex justify-content-between">
+        A third list item
+        <span class="badge bg-primary rounded-pill">1</span>
+      </li>
+      <li class="list-group-item d-flex justify-content-between">
+        A fourth list item
+        <span class="badge bg-primary rounded-pill">3</span>
+      </li>
+      <li class="list-group-item d-flex justify-content-between">
+        A fifth list item
+        <span class="badge bg-primary rounded-pill">7</span>
+      </li>
+      <li class="list-group-item d-flex justify-content-between">
+        <span class="bg-primary text-white">A sixth list item </span>
+        <span class="badge bg-primary rounded-pill">3</span>
+      </li>
+    </ul>
+  </div>
+</div>
+</div>
+</div>
+</div>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Dashboard</title>
+
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="css/dash.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+  <script src="js/popper.min.js"></script>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/dash.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-3 col-lg-3 bg-primary">
+        <ul class="nav flex-column">
+          <li class="nav-item spacy">
+            <a class="nav-link active" aria-current="page" href="#">
+              <i class="fa fa-buysellads text-white fa-3x"></i>
+              <span class="text-white h4 m-3">VIVALDI ADMIN</span>
+            </a>
+          </li>
+
+          <hr />
+
+          <li class="nav-item spacy">
+            <a class="nav-link" href="#">
+              <i class="fa fa-dashboard text-white fa-lg"></i>
+              <span class="text-white m-2">Dashboard</span>
+            </a>
+          </li>
+
+          <hr />
+
+          <li class="nav-item spacy">
+            <a class="nav-link" href="addparticipant.php">
+              <i class="fa fa-users text-white fa-lg"></i>
+              <span class="text-white m-2">Participants</span>
+            </a>
+          </li>
+          <li class="nav-item spacy">
+            <a class="nav-link" href="#">
+              <i class="fa fa-cogs text-white fa-lg"></i>
+              <span class="text-white m-2">Components</span>
+            </a>
+          </li>
+
+          <hr />
+
+          <li class="nav-item spacy">
+            <a class="nav-link" href="#">
+              <i class="fa fa-file text-white fa-lg"></i>
+              <span class="text-white m-2">Documents</span>
+            </a>
+          </li>
+          <li class="nav-item spacy">
+            <a class="nav-link" href="#">
+              <i class="fa fa-line-chart text-white fa-lg"></i>
+              <span class="text-white m-2">Charts</span>
+            </a>
+          </li>
+
+          <hr />
+
+          <li class="nav-item spacy">
+            <a class="nav-link" href="#">
+              <i class="fa fa-clipboard text-white fa-lg"></i>
+              <span class="text-white m-2">Accounts</span>
+            </a>
+          </li>
+          <li class="nav-item dropdown spacy">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <i class="fa fa-archive text-white fa-lg"></i>
+              <span class="text-white m-2">Archive</span>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarDarkDropdownMenuLink">
+              <li><a class="dropdown-item" href="#">2021</a></li>
+              <li><a class="dropdown-item" href="#">2020</a></li>
+            </ul>
+          </li>
+          <hr />
+          <li class="nav-item spacy">
+            <a class="nav-link" href="logout.php">
+              <i class="fa fa-mail-forward text-white fa-lg"></i>
+              <span class="text-white m-2">Log Out</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-sm-9 col-lg-9 bg-light">
+        <div class="row bg-white">
+          <div class="col-sm-7 col-lg-7">
+            <nav class="navbar navbar-light">
+              <div class="container-fluid">
+                <form class="d-flex">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                  <button class="btn btn-outline-primary" type="submit">
+                    Search
+                  </button>
+                </form>
+              </div>
+            </nav>
+          </div>
+
+          <div class="col-sm-5 col-lg-5">
+            <nav class="navbar navbar-expand-lg navbar-light">
+              <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                  <ul class="navbar-nav">
+                    <li class="nav-item">
+                      <a class="nav-link active btn dropdown-toggle" aria-current="page" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fa fa-envelope fa-lg grey"></i>
+                        <span class="badge bg-danger rounded-pill">10+</span>
+                      </a>
+                      <div class="dropdown-menu p-4 text-muted" style="max-width: 200px">
+                        <p>
+                          <b>John D.</b><br />
+                          Hello, could you pass by my office?
+                        </p>
+                        <p class="mb-0">
+                          <b>Molly C.</b><br />
+                          The report doesn't make sense.
+                        </p>
+                      </div>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">
+                        <i class="fa fa-bell fa-lg grey"></i>
+                        <span class="badge bg-danger rounded-pill">4</span>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">
+                        <span class="m-2"><?php echo $firstname; ?></span>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#" id="LiveToastBtn">
+                        <img class="rounded-circle" src="<?php echo $picture; ?>" alt="Loading" width="32" height="32" />
+                      </a>
+
+                      <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
+                        <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
+                          <div class="toast-header">
+                            <img src="..." class="rounded me-2" alt="..." />
+                            <strong class="me-auto">Bootstrap</strong>
+                            <small>11 mins ago</small>
+                            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                          </div>
+                          <div class="toast-body">
+                            Hello, world! This is a toast message.
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
